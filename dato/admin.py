cc615a44ce84f7bee6db9210a443be051a145434
Eduardo Rodríguez <xeduardox54@hotmail.com>
2021-01-08 12:46:09 -0500

    Datos con Arduino

--- dato/admin.py
+++ dato/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 
-from .models import Ubicacion
 from .models import Datos
 
-admin.site.register(Ubicacion)
 admin.site.register(Datos)
 
 # Register your models here.
